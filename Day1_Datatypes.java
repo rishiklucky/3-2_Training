17e1790ad7e5ec1251d7a09883bb2a24168dde6d
rishik05 <151130872+rishiklucky@users.noreply.github.com>
2025-12-10 15:27:40 +0530

    Add files via upload

---
+++ Day1_Datatypes.java
@@ -0,0 +1,15 @@
+public class Day1_Datatypes {
+     public static void main(String[] args) {
+        byte a = 127;
+        short b = 32767;
+        int c = 2147483647;
+        long d = 922337209;
+        float e = 1534545434;
+        double f = 1240544.6665;
+        boolean g = false;
+        char i = 'A';
+        String j = "Hi";
+        System.out.println(g);
+
+    }
+}
