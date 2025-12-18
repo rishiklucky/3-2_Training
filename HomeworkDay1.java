681624b7a02ede0594221283a585cb2f5c9b57de
rishik05 <151130872+rishiklucky@users.noreply.github.com>
2025-12-18 15:19:00 +0530

    Add files via upload

---
+++ HomeworkDay1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class HomeworkDay1 {
+
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        //Names
+        String student1 = "abhi";
+        String student2 = "balu";
+        String student3 = "chiru";
+
+        //Age
+        int student1_age = 19;
+        int student2_age = 20;
+        int student3_age = 19;
+
+        //Marks
+        int student1_marks = 90;
+        int student2_marks = 80;
+        int student3_marks = 70;
+
+        //Average Marks
+        int avg = (student1_marks+student2_marks+student3_marks)/3;
+        System.out.println("Average marks : "+avg);
+
+
+        }
+
+    }
+
